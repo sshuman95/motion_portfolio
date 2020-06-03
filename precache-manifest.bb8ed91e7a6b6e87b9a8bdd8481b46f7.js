a57eb789e5a33e8de3a80ccfec413e4aaad9f74c
unknown <sidney.shuman95@gmail.com>
2020-06-03 08:57:55 -0500

    Updates

---
+++ precache-manifest.bb8ed91e7a6b6e87b9a8bdd8481b46f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f393f26d8d50f03f759d3e3e4c5418ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c0271c40d772984c90c",
+    "url": "/static/css/main.4072d11c.chunk.css"
+  },
+  {
+    "revision": "d0f6af27d633f90debea",
+    "url": "/static/js/2.a75f3e1f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.a75f3e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c0271c40d772984c90c",
+    "url": "/static/js/main.17fd9223.chunk.js"
+  },
+  {
+    "revision": "3701b517b441343b5e1f",
+    "url": "/static/js/runtime-main.8c7c3bed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
