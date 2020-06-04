365188aa3119b6decf6f5b913223f00d8e879f82
unknown <sidney.shuman95@gmail.com>
2020-06-04 06:58:17 -0500

    changing slide sensitivity

--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -101,9 +101,9 @@ const Projects = () =>{
             dragConstraints={{ left: 0, right: 0 }}
             onDrag={
                 (event, info) => {
-                    if(info.point.x>50){
+                    if(info.point.x>40){
                         changeProject();
-                    } else if(info.point.x<-50){
+                    } else if(info.point.x<-40){
                         prevProject();
                     }
                 }
